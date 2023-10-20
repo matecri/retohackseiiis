ab72df83d5624f4a43f01f5000b4396a91e6b4f1
ValeAyala08 <valentina.ayala@est.iudigital.edu.co>
2023-10-20 10:21:32 -0500

    create init componets

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+
+}
